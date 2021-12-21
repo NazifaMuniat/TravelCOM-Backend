65fa6b7d15c9e4dde05236725dff88000a5dd583
Farjana03 <farzana.toma@northsouth.edu>
2021-12-22 00:22:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
 
 app.use("/", userRoutes);
 app.use("/tour", tourRoutes);
